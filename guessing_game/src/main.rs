84d91427e381919ed3bad52b85d1e4346e59a1ec
seunghyun-yeo <onyshup@gmail.com>
2022-08-23 04:25:08 -0400

    How to use .toml and RNG
    Add RNG to main.rs and dependency to Cargo.toml

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,11 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the nubmer!");
+    let secret_number = rand::thread_rng().gen_range(1..=10);
+    println!("Secret number is {secret_number}");
+    
     println!("Please input your guess.");
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("Failed to read line");
