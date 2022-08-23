d6f6136f07cca85198cc83474cbbd72e9c95ef5e
seunghyun-yeo <onyshup@gmail.com>
2022-08-23 04:53:42 -0400

    Geussing game explain

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,12 +4,21 @@ use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the nubmer!");
+    // generate random number in between 1 ~ 100
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("Secret number is {secret_number}");
     loop{
         println!("Please input your guess.");
+        
+        // create mutable variable named guess with string type
         let mut guess = String::new();
+
+        // read line from console and store it to guess, if it fails print string passed to .expect()
         io::stdin().read_line(&mut guess).expect("Failed to read line");
+        
+        // use shadow variable for guess and set it as unsigned 32bit inteager
+        // make string guess empty and change it into different type of var ie. inteager
+        // match will check the return of guess.trim().parse()
         let guess:u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
@@ -17,6 +26,7 @@ fn main() {
         
         println!("You guessed: {guess}");
         
+        // compare guess and secret_number
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
